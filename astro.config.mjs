87ed35265cda4b26872c3d4de8fb539d4bac9500
Nirmal H <nirmalh@Nirmals-MacBook-Pro.local>
2024-05-19 00:08:03 +0530

    feat: Added skills section - Generic mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
-import vue from "@astrojs/vue";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
